98aa0267b5c47e37e2cd163aac065f561ff0ef20
artenidus2012 <artemijka2@gmail.com>
2026-04-26 16:26:51 +0700

    появился на свет славный воин с ярой яростью

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from abc import ABC, abstractmethod
 
 
 class Character(ABC):
-    def __init__(self, name, hp, attack, defense, exp, level):
+    def __init__(self, name, hp, attack, defense, exp, level, creet_chans):
         self.name = name
         self.hp = hp
         self.attack = attack
@@ -12,6 +12,7 @@ class Character(ABC):
         self.exp = exp
         self.level = level
         self._inventory = []
+        self.creet_chans = creet_chans
 
     @abstractmethod
     def attack_target(self, target):
@@ -55,3 +56,40 @@ class Character(ABC):
             for e in self._inventory:
                 i += 1
                 print(f"{i}.{e}")
+
+    def __str__(self):
+        return f'твоё имя - {self.name} у вас {self.hp} здоровьья вы бьёте с силой {self.attack} у вас такая защита {self.defense} у вас столко опыта {self.exp}и вы на {self.level} левле'
+
+
+class Item(ABC):
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+
+    @abstractmethod
+    def use(self, character):
+        pass
+
+    def __str__(self):
+        return f'{self.name} {self.description}'
+
+
+class Warrior(Character):
+    def __init__(self, name):
+        super().__init__(name, hp=6, attack=3, defense=3, creet_chans=13)
+        self.rage = 0
+
+    def get_attack_power(self):
+        return self.attack
+
+    def attack_target(self, target):
+        dmg = self.get_attack_power()
+        creet = random.randint(1, 100)
+        if creet <= self.creet_chans and self.rage == 50:
+            target.hp -= dmg * 2 * 2
+            self.rage += 1
+        elif self.rage == 50 or creet <= self.creet_chans:
+            target.hp -= dmg * 2
+        else:
+            target.hp -= dmg
+            self.rage += 1
